4d771a9472be83730f306b40e788c19b0fccbb9e
unknown <arielkoh02@gmail.comgit config --global user.name Ariel>
2025-02-27 14:29:33 +0000

    cleaning timeseries data and plotting raw data

---
+++ code/code_raw_relationships_monitoring_data.R
@@ -0,0 +1,122 @@
+#26/2/25
+#plotting relationships using monitoring data
+
+#library 
+library(tidyverse)
+#install.packages("tidyverse")
+
+#loading data
+monitoring_data<-read.csv("data/raw/csv/monitoring_data_1980-2023.csv")
+
+#checking data
+View(monitoring_data)
+
+#changing the date format
+monitoring_data<-monitoring_data %>% 
+  mutate(Expr1=dmy(Expr1),
+         year = year(Expr1),  # Extract year
+         month = month(Expr1))
+
+View(monitoring_data)
+
+#filtering data for only RB5 data
+RB5_only <- monitoring_data %>%
+  filter(!is.na(Expr1) & Expr1 >as.Date("1986-04-07")) %>%   # Keep rows where Expr1 is not NA
+  select(Expr1, month, year, starts_with("RB5"))   # Keep Expr1 and columns starting with RB5
+
+#checking data
+View(RB5_only)
+
+#averaging across months 
+RB5_monthly_average<- RB5_only %>%
+  group_by(year, month) %>%
+  summarise(across(where(is.numeric), mean, na.rm = TRUE)) %>%  # Average numeric cols
+  ungroup()%>%
+  mutate(YearMonth = as.Date(paste(year, month, "01", sep = "-")))  # Create a proper date format
+
+#checking data 
+View(RB5_monthly_average)
+
+#plot SRP vs ChlA
+plot_SRPChlA<-ggplot(RB5_monthly_average, aes(x = RB5.SRP, y = RB5.ChlA)) +
+  geom_line(colour="lightblue",linewidth = 0.5) +  # Line plot
+  geom_point(colour="grey",size = 1) +  # Add points
+  geom_smooth(linetype="dashed",se=F,colour="black")+
+  labs(title = "Soluble Reactive Phosphorus vs Chlorophyll A",
+       x = "SRP (Average)",
+       y = "ChlA (Average)") +
+  theme_classic()  # Clean theme
+
+plot_SRPChlA
+
+ggsave("output/monitoringdatarelationship/SRPChlA.png",plot_SRPChlA,width=16,height=8,dpi=450)
+
+#plot NO3 vs ChlA
+plot_NO3ChlA<-ggplot(RB5_monthly_average, aes(x = RB5.NO3, y = RB5.ChlA)) +
+  geom_line(colour="lightblue",linewidth = 0.5) +  # Line plot
+  geom_point(colour="grey",size = 1) +  # Add points
+  geom_smooth(linetype="dashed",se=F,colour="black")+
+  labs(title = "Nitrate vs Chlorophyll A",
+       x = "NO3 (Average)",
+       y = "ChlA (Average)") +
+  theme_classic()  # Clean theme
+
+plot_NO3ChlA
+
+ggsave("output/monitoringdatarelationship/NO3ChlA.png",plot_NO3ChlA,width=16,height=8,dpi=450)
+
+#plot temp vs ChlA
+plot_tempChlA<-ggplot(RB5_monthly_average, aes(x = RB5.Temp, y = RB5.ChlA)) +
+  geom_line(colour="lightblue",linewidth = 0.5) +  # Line plot
+  geom_point(colour="grey",size = 1) +  # Add points
+  geom_smooth(linetype="dashed",se=F,colour="black")+
+  labs(title = "Temperature vs Chlorophyll A",
+       x = "Temperature (Average)",
+       y = "ChlA (Average)") +
+  theme_classic()  # Clean theme
+
+plot_tempChlA
+
+ggsave("output/monitoringdatarelationship/tempChlA.png",plot_tempChlA,width=16,height=8,dpi=450)
+
+#plot secchi depth vs ChlA
+plot_SDChlA<-ggplot(RB5_monthly_average, aes(x = RB5.SD, y = RB5.ChlA)) +
+  geom_line(colour="lightblue",linewidth = 0.5) +  # Line plot
+  geom_point(colour="grey",size = 1) +  # Add points
+  geom_smooth(linetype="dashed",se=F,colour="black")+
+  labs(title = "Secchi Depth vs Chlorophyll A",
+       x = "Secchi Depth (Average)",
+       y = "ChlA (Average)") +
+  theme_classic()  # Clean theme
+
+plot_SDChlA
+
+ggsave("output/monitoringdatarelationship/SDChlA.png",plot_tempChlA,width=16,height=8,dpi=450)
+
+#plot SRSi vs ChlA
+plot_SRSiChlA<-ggplot(RB5_monthly_average, aes(x = RB5.SRSi, y = RB5.ChlA)) +
+  geom_line(colour="lightblue",linewidth = 0.5) +  # Line plot
+  geom_point(colour="grey",size = 1) +  # Add points
+  geom_smooth(linetype="dashed",se=F,colour="black")+
+  labs(title = "Soluble Silica vs Chlorophyll A",
+       x = "Soluble Silica (Average)",
+       y = "ChlA (Average)") +
+  theme_classic()  # Clean theme
+
+plot_SRSiChlA
+
+ggsave("output/monitoringdatarelationship/SRSiChlA.png",plot_SRSiChlA,width=16,height=8,dpi=450)
+
+#plot DO vs ChlA
+plot_DOChlA<-ggplot(RB5_monthly_average, aes(x = RB5.DO, y = RB5.ChlA)) +
+  geom_line(colour="lightblue",linewidth = 0.5) +  # Line plot
+  geom_point(colour="grey",size = 1) +  # Add points
+  geom_smooth(linetype="dashed",se=F,colour="black")+
+  labs(title = "Dissolved Oxygen vs Chlorophyll A",
+       x = "Dissolved Oxygen (Average)",
+       y = "ChlA (Average)") +
+  theme_classic()  # Clean theme
+
+plot_DOChlA
+
+ggsave("output/monitoringdatarelationship/DOChlA.png",plot_DOChlA,width=16,height=8,dpi=450)
---
+++ code/code_raw_timeseries_data.R
@@ -0,0 +1,143 @@
+#7/2/25
+#plotting raw time series data 
+
+#library 
+library(tidyverse)
+#install.packages("tidyverse")
+
+#loading data
+monitoring_data<-read.csv("data/raw/csv/monitoring_data_1980-2023.csv")
+
+#checking data
+View(monitoring_data)
+
+#changing the date format
+monitoring_data<-monitoring_data %>% 
+  mutate(Expr1=dmy(Expr1),
+         year = year(Expr1),  # Extract year
+         month = month(Expr1))
+
+View(monitoring_data)
+
+#filtering data for only RB5 data
+RB5_only <- monitoring_data %>%
+  filter(!is.na(Expr1) & Expr1 >as.Date("1986-04-07")) %>%   # Keep rows where Expr1 is not NA
+  select(Expr1, month, year, starts_with("RB5"))   # Keep Expr1 and columns starting with RB5
+
+#checking data
+View(RB5_only)
+
+#averaging across months 
+RB5_monthly_average<- RB5_only %>%
+  group_by(year, month) %>%
+  summarise(across(where(is.numeric), mean, na.rm = TRUE)) %>%  # Average numeric cols
+  ungroup()%>%
+  mutate(YearMonth = as.Date(paste(year, month, "01", sep = "-")))  # Create a proper date format
+
+#checking data 
+View(RB5_monthly_average)
+
+#plot ChlA
+plot_ChlA<-ggplot(RB5_monthly_average, aes(x = YearMonth, y = RB5.ChlA)) +
+  geom_line(colour="lightblue",linewidth = 0.5) +  # Line plot
+  geom_point(colour="grey",size = 1) +  # Add points
+  geom_smooth(linetype="dashed",se=F,colour="black")+
+  labs(title = "Time Series of RB5_ChlA",
+       x = "Year-Month",
+       y = "Average Chlorophyll A Content") +
+  scale_x_date(date_labels = "%b %Y", date_breaks = "6 months") +  # Format x-axis
+  theme_minimal()  # Clean theme
+
+plot_ChlA
+
+ggsave("output/timeseries/chlA.png",plot_ChlA,width=16,height=8,dpi=450)
+
+#plot SRP
+plot_SRP<-ggplot(RB5_monthly_average, aes(x = YearMonth, y = RB5.SRP)) +
+   geom_line(colour="lightblue",linewidth = 0.5) +  # Line plot
+   geom_point(colour="grey",size = 1) +  # Add points
+   geom_smooth(linetype="dashed",se=F,colour="black")+
+   labs(title = "Time Series of Soluble Reactive Phosphorus (1986-2023)",
+        x = "Year-Month",
+        y = "Average SRP") +
+   scale_x_date(date_labels = "%Y", date_breaks = "12 months") +  # Format x-axis
+   theme_classic()  # Clean theme
+
+plot_SRP
+
+ggsave("output/timeseries/SRP.png",plot_SRP,width=16,height=8,dpi=450)
+
+#plot NO3
+plot_NO3<-ggplot(RB5_monthly_average, aes(x = YearMonth, y = RB5.NO3)) +
+   geom_line(colour="lightblue",linewidth = 0.5) +  # Line plot
+   geom_point(colour="grey",size = 1) +  # Add points
+   geom_smooth(linetype="dashed",se=F,colour="black")+
+   labs(title = "Time Series of Nitrate Content (1986-2023)",
+        x = "Year-Month",
+        y = "Average Nitrate Content") +
+   scale_x_date(date_labels = "%Y", date_breaks = "12 months") +  # Format x-axis
+   theme_classic()  # Clean theme
+
+ plot_NO3
+ 
+ggsave("output/timeseries/NO3.png",plot_NO3,width=16,height=8,dpi=450)
+ 
+#plot temp
+plot_temp<-ggplot(RB5_monthly_average, aes(x = YearMonth, y = RB5.Temp)) +
+  geom_line(colour="lightblue",linewidth = 0.5) +  # Line plot
+  geom_point(colour="grey",size = 1) +  # Add points
+  geom_smooth(linetype="dashed",se=F,colour="black")+
+  labs(title = "Time Series of Temperature (1986-2023)",
+       x = "Year-Month",
+       y = "Average Temperature") +
+  scale_x_date(date_labels = "%Y", date_breaks = "12 months") +  # Format x-axis
+  theme_classic()  # Clean theme
+
+plot_temp
+
+ggsave("output/timeseries/temp.png",plot_NO3,width=16,height=8,dpi=450)
+
+#plot secchi depth
+plot_SD<-ggplot(RB5_monthly_average, aes(x = YearMonth, y = RB5.SD)) +
+  geom_line(colour="lightblue",linewidth = 0.5) +  # Line plot
+  geom_point(colour="grey",size = 1) +  # Add points
+  geom_smooth(linetype="dashed",se=F,colour="black")+
+  labs(title = "Time Series of Secchi Depth (1986-2023)",
+       x = "Year-Month",
+       y = "Average Secchi Depth") +
+  scale_x_date(date_labels = "%Y", date_breaks = "12 months") +  # Format x-axis
+  theme_classic()  # Clean theme
+
+plot_SD
+
+ggsave("output/timeseries/SD.png",plot_NO3,width=16,height=8,dpi=450)
+
+#plot soluble reactive silica
+plot_SRSi<-ggplot(RB5_monthly_average, aes(x = YearMonth, y = RB5.SRSi)) +
+  geom_line(colour="lightblue",linewidth = 0.5) +  # Line plot
+  geom_point(colour="grey",size = 1) +  # Add points
+  geom_smooth(linetype="dashed",se=F,colour="black")+
+  labs(title = "Time Series of Soluble Reactive Silica (1986-2023)",
+       x = "Year-Month",
+       y = "Average Soluble Reactive Silica") +
+  scale_x_date(date_labels = "%Y", date_breaks = "12 months") +  # Format x-axis
+  theme_classic()  # Clean theme
+
+plot_SRSi
+
+ggsave("output/timeseries/SRSi.png",plot_SRSi,width=16,height=8,dpi=450)
+
+#plot dissolved oxygen
+plot_DO<-ggplot(RB5_monthly_average, aes(x = YearMonth, y = RB5.DO)) +
+  geom_line(colour="lightblue",linewidth = 0.5) +  # Line plot
+  geom_point(colour="grey",size = 1) +  # Add points
+  geom_smooth(linetype="dashed",se=F,colour="black")+
+  labs(title = "Time Series of Dissolved Oxygen (1986-2023)",
+       x = "Year-Month",
+       y = "Average Dissolved Oxygen") +
+  scale_x_date(date_labels = "%Y", date_breaks = "12 months") +  # Format x-axis
+  theme_classic()  # Clean theme
+
+plot_DO
+
+ggsave("output/timeseries/DO.png",plot_DO,width=16,height=8,dpi=450)
