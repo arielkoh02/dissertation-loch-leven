2ce21542e2c18f49404a9bd7ce0f6f82a4888fa5
unknown <arielkoh02@gmail.comgit config --global user.name Ariel>
2025-03-05 16:18:06 +0000

    seasonal phytoplankton biovol plots

--- code/code_phytoplankton_biovolumes.R
+++ code/code_phytoplankton_biovolumes.R
@@ -1,14 +1,16 @@
 #3/3/25
 #plotting phytoplankton biovolumes
 
+rm(list=ls())
+
 #library 
 library(tidyverse)
 
 #loading data
-cryto_data<-read.csv("data/raw/csv/cryto_phyto_2004-2016.csv")
-cyano_data<-read.csv("data/raw/csv/cyano_phyto_2004-2016.csv")
-diatoms_data<-read.csv("data/raw/csv/diatoms_phyto_2004-2016.csv")
-greens_data<-read.csv("data/raw/csv/greens_phyto_2004-2016.csv")
+cryto_data<-read.csv("data/csv/cryto_phyto_2004-2016.csv")
+cyano_data<-read.csv("data/csv/cyano_phyto_2004-2016.csv")
+diatoms_data<-read.csv("data/csv/diatoms_phyto_2004-2016.csv")
+greens_data<-read.csv("data/csv/greens_phyto_2004-2016.csv")
 
 View(cryto_data)
 
@@ -107,3 +109,102 @@ plot_biovolchla
 
 ggsave("output/chlaproxy/biovolvschla.png",plot_biovolchla,width=16,height=8,dpi=450)
 
+
+phyto_seasonal_average <- biovol_fulldata %>%
+  mutate(
+    season = case_when(
+      month %in% c(12) ~ "Winter",    # December is Winter
+      month %in% c(1, 2) ~ "Winter",  # January & February are also Winter
+      month %in% c(3, 4, 5) ~ "Spring",
+      month %in% c(6, 7, 8) ~ "Summer",
+      month %in% c(9, 10, 11) ~ "Autumn"
+    ),
+    season_year = if_else(month == 12, year + 1, year)  # Shift December’s year forward
+  ) %>%
+  group_by(season_year, season) %>%
+  summarise(across(ends_with("Biovolume"), mean, na.rm = TRUE), .groups = "drop")  # Averages all RB5.* columns
+
+# Assign numeric months for correct date alignment
+phyto_seasonal_average <- phyto_seasonal_average %>%
+  mutate(
+    month_for_plot = case_when(
+      season == "Winter" ~ 1,   # Assign January as representative of Winter
+      season == "Spring" ~ 4,   # April for Spring
+      season == "Summer" ~ 7,   # July for Summer
+      season == "Autumn" ~ 10   # October for Autumn
+    ),
+    date_for_plot = as.Date(paste(season_year, month_for_plot, "01", sep = "-")) # Create valid Date
+  )%>%
+  drop_na(season)
+
+View(phyto_seasonal_average)
+
+#plot seasonal cryto biovol
+plot_seasonalcryto <- ggplot(phyto_seasonal_average, aes(x = date_for_plot, y = Cryto.Biovolume)) +
+  geom_line(size = 0.5) +  # Line color for each Phytoplankton Group
+  geom_point(aes(colour = season), size = 2) +   # Point color based on Season
+  labs(title = "Seasonal Cryto Biovolume Trends",
+       x = "Year",
+       y = "Biovolume") +
+  theme_classic() +
+  scale_color_manual(values = c("Winter" = "lightblue", "Spring" = "lightgreen", 
+                                "Summer" = "maroon", "Autumn" = "orange")) +
+  scale_x_date(date_labels = "%Y", date_breaks = "1 year") +
+  guides(color = guide_legend(title = "Season"))
+
+plot_seasonalcryto
+
+#plot seasonal cyano biovol
+plot_seasonalcyano <- ggplot(phyto_seasonal_average, aes(x = date_for_plot, y = Cyano.Biovolume)) +
+  geom_line(size = 0.5) +  # Line color for each Phytoplankton Group
+  geom_point(aes(colour = season), size = 2) +   # Point color based on Season
+  labs(title = "Seasonal Cyano Biovolume Trends",
+       x = "Year",
+       y = "Biovolume") +
+  theme_classic() +
+  scale_color_manual(values = c("Winter" = "lightblue", "Spring" = "lightgreen", 
+                                "Summer" = "maroon", "Autumn" = "orange")) +
+  scale_x_date(date_labels = "%Y", date_breaks = "1 year") +
+  guides(color = guide_legend(title = "Season"))
+
+plot_seasonalcyano
+
+#plot seasonal diatoms biovol
+plot_seasonaldiatoms <- ggplot(phyto_seasonal_average, aes(x = date_for_plot, y = Diatoms.Biovolume)) +
+  geom_line(size = 0.5) +  # Line color for each Phytoplankton Group
+  geom_point(aes(colour = season), size = 2) +   # Point color based on Season
+  labs(title = "Seasonal Diatoms Biovolume Trends",
+       x = "Year",
+       y = "Biovolume") +
+  theme_classic() +
+  scale_color_manual(values = c("Winter" = "lightblue", "Spring" = "lightgreen", 
+                                "Summer" = "maroon", "Autumn" = "orange")) +
+  scale_x_date(date_labels = "%Y", date_breaks = "1 year") +
+  guides(color = guide_legend(title = "Season"))
+
+
+plot_seasonaldiatoms
+
+#plot seasonal greens biovol
+plot_seasonalgreens <- ggplot(phyto_seasonal_average, aes(x = date_for_plot, y = Greens.Biovolume)) +
+  geom_line(size = 0.5) +  # Line color for each Phytoplankton Group
+  geom_point(aes(colour = season), size = 2) +   # Point color based on Season
+  labs(title = "Seasonal Greens Biovolume Trends",
+       x = "Year",
+       y = "Biovolume") +
+  theme_classic() +
+  scale_color_manual(values = c("Winter" = "lightblue", "Spring" = "lightgreen", 
+                                "Summer" = "maroon", "Autumn" = "orange")) +
+  scale_x_date(date_labels = "%Y", date_breaks = "1 year") +
+  guides(color = guide_legend(title = "Season"))
+
+
+plot_seasonalgreens
+
+install.packages("ggpubr")
+library(ggpubr)
+
+# Arrange in a 2x2 grid
+plot_seasonalbiovol<-ggarrange(plot_seasonalcryto, plot_seasonalcyano, plot_seasonaldiatoms, plot_seasonalgreens, nrow = 2, ncol = 2, common.legend = TRUE, legend="bottom")
+plot_seasonalbiovol
+ggsave("output/seasonaltimeseries/biovol.png",plot_seasonalbiovol,width=16,height=8,dpi=450)
