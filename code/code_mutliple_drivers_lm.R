cea4d8aa852244886e9773738ebabcc539ccefef
unknown <arielkoh02@gmail.comgit config --global user.name Ariel>
2025-03-19 16:46:26 +0000

    roadmap for linear models + outputs

--- code/code_mutliple_drivers_lm.R
+++ code/code_mutliple_drivers_lm.R
@@ -4,16 +4,10 @@
 rm=list(ls)
 
 library(tidyverse)
-#install.packages("lme4")
-#library(lme4)
-#install.packages("lmerTest")
 library(lmerTest)
-#install.packages("DHARMa")
 library(DHARMa)
 
 alldata<-read.csv("data/csv/alldata-2004-2016.csv")
-
-View(alldata)
 
 alldata<-alldata %>% mutate(Total.Biovolume=log(Total.Biovolume))
 
@@ -24,35 +18,91 @@ alldatalong <- alldata %>%
   mutate(Biovolume= log(Biovolume)) %>% 
   filter(!(Phytoplankton_Group=='Total.Biovolume'))
 
+View(alldata)
+View(alldatalong)
+
 #NO3+SRP
-mod1 <- lmerTest::lmer(Total.Biovolume ~ RB5.SRP+RB5.NO3+(1|Year), data=alldata)
+mod1 <- lmerTest::lmer(Total.Biovolume ~ RB5.SRP+(1|Year), data=alldata)
 summary(mod1)
 
 mod1plot<-simulateResiduals(fittedModel = mod1, plot = F)
 plot(mod1plot)
 
-mod2 <- lmerTest::lmer(Biovolume ~ RB5.SRP + RB5.NO3 + Phytoplankton_Group + Season + (1 | Year), 
+mod2 <- lmerTest::lmer(Biovolume ~ RB5.SRP + Phytoplankton_Group + Season + (1 | Year), 
                           data = alldatalong)
 summary(mod2)
 
 mod2plot<-simulateResiduals(fittedModel = mod2, plot = F)
 plot(mod2plot)
 
-AIC(mod1,mod2)
-
-#NO3+SRP+Predator
-mod3 <- lmerTest::lmer(Total.Biovolume ~ RB5.SRP+RB5.NO3+Daphnia+(1|Year), data=alldata)
+mod3 <- lmerTest::lmer(Biovolume ~ RB5.SRP + Phytoplankton_Group*Season + (1 | Year), 
+                       data = alldatalong)
 summary(mod3)
 
 mod3plot<-simulateResiduals(fittedModel = mod3, plot = F)
 plot(mod3plot)
 
-mod4 <- lmerTest::lmer(Biovolume ~ RB5.SRP+RB5.NO3+Daphnia+Phytoplankton_Group + Season +(1|Year), data=alldatalong)
+AIC(mod1,mod2,mod3)
+
+#NO3+SRP
+mod4 <- lmerTest::lmer(Biovolume ~ RB5.SRP + RB5.NO3 + Phytoplankton_Group*Season + (1 | Year), 
+                       data = alldatalong)
 summary(mod4)
 
 mod4plot<-simulateResiduals(fittedModel = mod4, plot = F)
 plot(mod4plot)
 
-AIC(mod1,mod2,mod3)
+AIC(mod1,mod2,mod3,mod4)
+
+#NO3+SRP+Daphnia
+mod5 <- lmerTest::lmer(Biovolume ~ RB5.SRP + RB5.NO3 + Daphnia + Phytoplankton_Group*Season + (1 | Year), 
+                       data = alldatalong)
+summary(mod5)
+
+mod5plot<-simulateResiduals(fittedModel = mod5, plot = F)
+plot(mod5plot)
+
+AIC(mod1,mod2,mod3,mod4,mod5)
+
+#NO3+SRP+Daphnia+pH
+mod6 <- lmerTest::lmer(Biovolume ~ RB5.SRP + RB5.NO3 + Daphnia + RB5.pH + Phytoplankton_Group*Season + (1 | Year), 
+                       data = alldatalong)
+summary(mod6)
+
+mod6plot<-simulateResiduals(fittedModel = mod6, plot = F)
+plot(mod6plot)
+
+#NO3+SRP+Daphnia+pH+temp
+mod7 <- lmerTest::lmer(Biovolume ~ RB5.SRP + RB5.NO3 + Daphnia + RB5.pH + RB5.Temp + Phytoplankton_Group*Season + (1 | Year), 
+                       data = alldatalong)
+summary(mod7)
+
+mod7plot<-simulateResiduals(fittedModel = mod7, plot = F)
+plot(mod7plot)
+
+#NO3+SRP+Daphnia+pH+temp+water depth
+mod8 <- lmerTest::lmer(Biovolume ~ RB5.SRP + RB5.NO3 + Daphnia + RB5.pH + RB5.Temp + waterlevel.masl + Phytoplankton_Group*Season + (1 | Year), 
+                       data = alldatalong)
+summary(mod8)
+
+mod8plot<-simulateResiduals(fittedModel = mod8, plot = F)
+plot(mod8plot)
+
+#NO3+SRP+Daphnia+pH+temp+water depth+DO
+mod9 <- lmerTest::lmer(Biovolume ~ RB5.SRP + RB5.NO3 + Daphnia + RB5.pH + RB5.Temp + waterlevel.masl + RB5.DO + Phytoplankton_Group*Season + (1 | Year), 
+                       data = alldatalong)
+summary(mod9)
+
+mod9plot<-simulateResiduals(fittedModel = mod9, plot = F)
+plot(mod9plot)
+
+#NO3+SRP+Daphnia+pH+temp+water depth+DO+SD
+mod10 <- lmerTest::lmer(Biovolume ~ RB5.SRP + RB5.NO3 + Daphnia + RB5.pH + RB5.Temp + waterlevel.masl + RB5.DO + RB5.SD + Phytoplankton_Group*Season + (1 | Year), 
+                       data = alldatalong)
+summary(mod10)
+
+mod10plot<-simulateResiduals(fittedModel = mod10, plot = F)
+plot(mod10plot)
 
 
+AIC(mod1, mod2, mod3, mod4, mod5, mod6, mod7, mod8, mod9, mod10)
